16e6415edb8046a5f43245585f41763aebfc18d2
NetCommons Project <netcommons@netcommons.org>
2014-06-04 16:34:43 +0900

    Update define.inc.php

--- html/webapp/modules/comp/config/insertamazon/define.inc.php
+++ html/webapp/modules/comp/config/insertamazon/define.inc.php
@@ -11,12 +11,15 @@
  * @project     NetCommons Project, supported by National Institute of Informatics
  * @access      public
  */
+/* ---
+ * 2014-6-4 AWS ACCESS KEY 情報を削除しました
 define("COMP_XML_ENCODING", "UTF-8");
-define("COMP_AWS_ACCESS_KEY_ID", "AKIAILVYEX6M4DJCGHGA");
-define("COMP_AWS_SECRET_KEY_ID", "ndjGU+Nl0DFdDKPlzvLQsr5CZGduzMO1JtQwEhmJ");
-define("COMP_AWS_ASSOCIATE_TAG", "commonsnet109-22");
+define("COMP_AWS_ACCESS_KEY_ID", "****");
+define("COMP_AWS_SECRET_KEY_ID", "****");
+define("COMP_AWS_ASSOCIATE_TAG", "*****");
 define("COMP_AWS_URL", "http://webservices.amazon.co.jp/onca/xml");
 define("COMP_AWS_REST", "GET\nwebservices.amazon.co.jp\n/onca/xml\n%s");
 //define("COMP_AWS_VISIBLE_ROW", 10); //Amazonでは固定
 define("COMP_AWS_DEFAULT_PAGE_NEXT", 4);
+ */
 ?>
