c06abd188c5e04d4f52ae58b587d9915f23341bf
Ryuji Masukawa <masukawa@nii.ac.jp>
2018-01-17 18:52:29 +0900

    以下の不具合を修正 ①新規でToDoを作成 ②ToDoを追加（カレンダーに表示させる） ③セッティングモードでToDoリスト一覧表示の状態でリスト選択、「削除」ボタン押下 ④カレンダーの予定が削除されず残っている状態

--- html/webapp/modules/todo/action/edit/delete/Delete.class.php
+++ html/webapp/modules/todo/action/edit/delete/Delete.class.php
@@ -19,6 +19,7 @@ class Todo_Action_Edit_Delete extends Action
     // 使用コンポーネントを受け取るため
     var $todoAction = null;
     var $db = null;
+	var $calendarPlanAction = null;
 
     /**
      * Todo削除アクション
@@ -27,6 +28,19 @@ class Todo_Action_Edit_Delete extends Action
      */
     function execute()
     {
+		$whereParams = array(
+			"todo_id" => $this->todo_id,
+			"calendar_id!=0" => null
+		);
+		$tasks = $this->db->selectExecute('todo_task', $whereParams);
+		if(!empty($tasks)) {
+			foreach($tasks as $task) {
+				if (!$this->calendarPlanAction->deletePlan($task["calendar_id"], CALENDAR_PLAN_EDIT_THIS)) {
+					return false;
+				}
+			}
+		}
+
 		$whereParams = array(
 			"todo_id" => $this->todo_id
 		);
